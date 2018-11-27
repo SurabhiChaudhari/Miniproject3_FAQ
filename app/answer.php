e5ad5ca6a8baa25fbc2e5a345bb173259a118801
Surabhi Chaudhari <sc2263@njit.edu>
2018-11-27 14:45:44 -0500

    Task : Created answerForm page, added route and functions to create and store answer

--- app/answer.php
+++ app/answer.php
@@ -1,8 +1,10 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
+
 class Answer extends Model
 {
+    protected $fillable = ['body'];
     public function user()
     {
         return $this->belongsTo('App\User');
